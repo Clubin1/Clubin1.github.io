6a80884a5fad9a0bef6bf371b7a5c8f4bb715d30
justin tubay <justin.tubay@gmail.com>
2022-02-27 21:00:23 -0500

    calculator add

---
+++ ITIS3135/scripts/calculator_try.js
@@ -0,0 +1,101 @@
+let clickSeven = () => {
+    let currentOutput = document.getElementById("output").innerHTML
+    if (parseInt(currentOutput) === 0) {
+        document.getElementById("output").innerHTML = "7"
+    } else {
+        document.getElementById("output").innerHTML += "7"
+    }
+}
+
+let clickEight = () => {
+    let currentOutput = document.getElementById("output").innerHTML
+    if (parseInt(currentOutput) === 0) {
+        document.getElementById("output").innerHTML = "8"
+    } else {
+        document.getElementById("output").innerHTML += "8"
+    }
+}
+
+let clickNine = () => {
+    let currentOutput = document.getElementById("output").innerHTML
+    if (parseInt(currentOutput) === 0) {
+        document.getElementById("output").innerHTML = "9"
+    } else {
+        document.getElementById("output").innerHTML += " 9"
+    }
+}
+
+let clickSix = () => {
+    let currentOutput = document.getElementById("output").innerHTML
+    if (parseInt(currentOutput) === 0) {
+        document.getElementById("output").innerHTML = "6"
+    } else {
+        document.getElementById("output").innerHTML += "6"
+    }
+}
+
+let clickFive = () => {
+    let currentOutput = document.getElementById("output").innerHTML
+    if (parseInt(currentOutput) === 0) {
+        document.getElementById("output").innerHTML = "5"
+    } else {
+        document.getElementById("output").innerHTML += "5"
+    }
+}
+let clickFour = () => {
+    let currentOutput = document.getElementById("output").innerHTML
+    if (parseInt(currentOutput) === 0) {
+        document.getElementById("output").innerHTML = "4"
+    } else {
+        document.getElementById("output").innerHTML += "4"
+    }
+}
+
+let clickThree = () => {
+    let currentOutput = document.getElementById("output").innerHTML
+    if (parseInt(currentOutput) === 0) {
+        document.getElementById("output").innerHTML = "3"
+    } else {
+        document.getElementById("output").innerHTML += "3"
+    }
+}
+
+let clickTwo = () => {
+    let currentOutput = document.getElementById("output").innerHTML
+    if (parseInt(currentOutput) === 0) {
+        document.getElementById("output").innerHTML = "2"
+    } else {
+        document.getElementById("output").innerHTML += "2"
+    }
+}
+
+let clickOne = () => {
+    let currentOutput = document.getElementById("output").innerHTML
+    if (parseInt(currentOutput) === 0) {
+        document.getElementById("output").innerHTML = "1"
+    } else {
+        document.getElementById("output").innerHTML += "1"
+    }
+}
+
+let clickZero = () => {
+    let currentOutput = document.getElementById("output").innerHTML
+    if (parseInt(currentOutput) === 0) {
+        document.getElementById("output").innerHTML = "0"
+    } else {
+        document.getElementById("output").innerHTML += "0"
+    }
+}
+
+
+// Not sure how to do operations. I know I need to save my current item
+/* 
+it would be like document.getElementById("output").innerHTML
+wait for equal sign and get second number,
+then update ID with result.
+typeNum()
+firstOpperand()
+typeSecond()
+equal()
+I believe I understand the logic, just not how to get there. 
+*/
